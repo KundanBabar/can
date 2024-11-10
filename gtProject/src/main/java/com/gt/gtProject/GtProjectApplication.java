1a8c090f060d78bfa566e988d4b731dfe77c5c3d
KUNDAN <KUNDAN@DESKTOP-B1A9P57.mshome.net>
2024-11-10 02:07:18 -0800

    first commit

---
+++ gtProject/src/main/java/com/gt/gtProject/GtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gt.gtProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtProjectApplication.class, args);
+	}
+
+}
