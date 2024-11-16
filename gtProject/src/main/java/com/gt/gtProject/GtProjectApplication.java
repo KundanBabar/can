e07a08143b6aaf26059cc850ce597c69ac125be0
Kundan <Kundan@DESKTOP-ETHKAAR>
2024-11-16 22:56:31 +0530

    s

--- gtProject/src/main/java/com/gt/gtProject/GtProjectApplication.java
+++ gtProject/src/main/java/com/gt/gtProject/GtProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GtProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GtProjectApplication.class, args);
+		SpringApplication.run( GtProjectApplication.class, args);
 	} 
 
 }
